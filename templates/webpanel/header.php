eb4e4c47e90cb15a69c144f4dfabb0e701285233
andriux26 <andriux26@gmail.com>
2025-01-16 18:21:02 +0200

    Update header.php

--- templates/webpanel/header.php
+++ templates/webpanel/header.php
@@ -15,7 +15,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" type="text/css" href="style.css">
-  <title><?= isset($PageTitle) ? $PageTitle : "Raspberry NOAA"?></title>
+  <title><?= isset($PageTitle) ? $PageTitle : "Raspberry ISS"?></title>
 </head>
 <body>
 <div class="topnav">
