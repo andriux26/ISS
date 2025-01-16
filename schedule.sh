dd957bdaf54e5d595cb1ccd4b4e530e845c76a30
andriux26 <andriux26@gmail.com>
2025-01-16 17:55:13 +0200

    Update schedule.sh

--- schedule.sh
+++ schedule.sh
@@ -8,13 +8,13 @@ fi
 
 ## import common lib
 . "$HOME/.noaa.conf"
-. "$NOAA_HOME/common.sh"
-wget -qr http://www.celestrak.com/NORAD/elements/weather.txt -O "${NOAA_HOME}"/predict/weather.txt
-wget -qr http://www.celestrak.com/NORAD/elements/amateur.txt -O "${NOAA_HOME}"/predict/amateur.txt
-grep "NOAA 15" "${NOAA_HOME}"/predict/weather.txt -A 2 > "${NOAA_HOME}"/predict/weather.tle
+. "$ISS_HOME/common.sh"
+wget -qr http://www.celestrak.com/NORAD/elements/weather.txt -O "${ISS_HOME}"/predict/weather.txt
+wget -qr http://www.celestrak.com/NORAD/elements/amateur.txt -O "${ISS_HOME}"/predict/amateur.txt
+grep "NOAA 15" "${ISS_HOME}"/predict/weather.txt -A 2 > "${ISS_HOME}"/predict/weather.tle
 
 if [ "$SCHEDULE_ISS" == "true" ]; then
-    grep "ZARYA" "${NOAA_HOME}"/predict/amateur.txt -A 2 > "${NOAA_HOME}"/predict/amateur.tle
+    grep "ZARYA" "${ISS_HOME}"/predict/amateur.txt -A 2 > "${ISS_HOME}"/predict/amateur.tle
 
 
    
@@ -30,6 +30,6 @@ for i in $(atq | awk '{print $1}');do atrm "$i";done
 
 #Schedule Satellite Passes:
 if [ "$SCHEDULE_ISS" == "true" ]; then
-    "${NOAA_HOME}"/schedule_iss.sh "ISS (ZARYA)" 437.8000
+    "${ISS_HOME}"/schedule_iss.sh "ISS (ZARYA)" 437.8000
 fi
 #"${NOAA_HOME}"/schedule_sat.sh "NOAA-15" 137.6200
